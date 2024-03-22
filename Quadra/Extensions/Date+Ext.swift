a2d66b01148ccdd52a32bba18a84283bb5f818bf
Tatyana Balashenko <balashenko.tatyana@gmail.com>
2024-03-22 17:33:11 +0100

    Add CardManager, clean up

---
+++ Quadra/Extensions/Date+Ext.swift
@@ -0,0 +1,8 @@
+//
+//  Date+Ext.swift
+//  Quadra
+//
+//  Created by Tatyana Balashenko on 22/03/2024.
+//
+
+import Foundation
---
+++ Quadra/Model/CardManager.swift
@@ -0,0 +1,8 @@
+//
+//  CardManager.swift
+//  Quadra
+//
+//  Created by Tatyana Balashenko on 22/03/2024.
+//
+
+import Foundation
