fa54b1ad9cca09e779f34291dd8c40408ada9481
Tatyana Balashenko <balashenko.tatyana@gmail.com>
2024-03-27 22:26:26 +0100

    Some UI fixes, clean up, add button to move cards, set up Data Manager

--- Quadra/Model/CardManager.swift
+++
@@ -1,8 +0,0 @@
-//
-//  CardManager.swift
-//  Quadra
-//
-//  Created by Tatyana Balashenko on 22/03/2024.
-//
-
-import Foundation
---
+++ Quadra/View/ListRow.swift
@@ -0,0 +1,18 @@
+//
+//  ListRow.swift
+//  Quadra
+//
+//  Created by Tatyana Balashenko on 27/03/2024.
+//
+
+import SwiftUI
+
+struct ListRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListRow()
+}
